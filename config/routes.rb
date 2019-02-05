4ac66041494599aa0abaf0114161ebab34f10122
guillaume-CHR <guillaume.christe@gmail.com>
2019-02-05 16:58:11 +0100

    Upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'user/show'
-  get 'user/edit'
-  get 'user/new'
-  get 'user/destroy'
-  get 'author/show'
-  get 'author/edit'
-  get 'author/new'
-  get 'author/destroy'
-  get 'gossip/show'
-  get 'gossip/edit'
-  get 'gossip/new'
-  get 'gossip/destroy'
 	#Home Page: Identification > Customized Home Page
-	root 'home#index'
-	get '/welcome/:text', to: 'home#welcome'
+	root 'homes#index'
+	get '/welcome/:text', to: 'homes#welcome'
 
 	#Specific Pages
-	get '/team', to: 'home#team'
-	get '/contact', to: 'home#contact'
+	get '/team', to: 'homes#team'
+	get '/contact', to: 'homes#contact'
 
 	#Gossips
-	get '/gossips/:gossip_id', to: 'gossip#show'
-	get '/gossips/:gossip_id/edit', to: 'gossip#edit'
-	get '/gossips/new', to: 'gossip#new'
-	get '/gossips/new', to: 'gossip#destroy'
+	get '/gossips/:gossip_id', to: 'gossips#show'
+	get '/gossips/:gossip_id/edit', to: 'gossips#edit'
+	get '/gossips/new', to: 'gossips#new'
+	get '/gossips/new', to: 'gossips#destroy'
 
 	#User
-	get '/users/:user_id', to: 'user#show'
-	get '/users/:gossip_id/edit', to: 'user#edit'
-	get '/users/new', to: 'user#new'
-	get '/users/new', to: 'user#destroy'
+	get '/users/:user_id', to: 'users#show'
+	get '/users/:gossip_id/edit', to: 'users#edit'
+	get '/users/new', to: 'users#new'
+	get '/users/new', to: 'users#destroy'
 
 end
